b467640d87eb5c6480eaae498b83879b62eb3e5d
abhisahni00 <abhisheksahni011@gmail.com>
2024-10-28 13:40:35 +0530

    permission2

--- app/src/main/java/com/example/Basics/CameraPermission.kt
+++ app/src/main/java/com/example/Basics/CameraPermission.kt
@@ -14,25 +14,43 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CameraPermission : AppCompatActivity() {
+    var c=1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_camera_permission)
         val b1=findViewById<androidx.cardview.widget.CardView>(R.id.card1)
-        var c=1
+
 
         b1.setOnClickListener{
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)== PackageManager.PERMISSION_DENIED){
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),c)
-
-
             }
             else{
-                val i6 = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivity(i6)
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivity(intent)
                 Toast.makeText(this, "Permission Already Granted", Toast.LENGTH_SHORT).show()
             }
         }
 
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == c){
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivity(intent)
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
